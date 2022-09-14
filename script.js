59e040c63e882d20a438db529463cf4505865fcd
shariful4345 <mdsajeeb4345@gmail.com>
2022-09-14 22:45:49 +0600

    added count time second

--- script.js
+++ script.js
@@ -133,8 +133,11 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpent = Math.floor((currentTime - startTime) / 1000);
 
-
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById('show-time').innerHTML = `${startTime ? timeSpent : 0
+    } seconds`;
 }, 1000);
+
+
+
